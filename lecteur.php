591febe175a523fae05bc27006415532bd00a8d9
RomainMustang <romain.g@codeur.online>
2017-01-30 09:36:00 +0100

    Liste les différents fichiers et indique si le fichier est un fichier ou un dossier

--- lecteur.php
+++ lecteur.php
@@ -1,12 +1,66 @@
 <?php
 
-$dir = 'C:/wamp64/www';
-$files = scandir($dir);
-
-
-for($i = 0 ; $i < count($files); $i++)
+if(!isset($_GET['path']))
 {
-	echo $files[$i] . "<br>";
+	$repertoire = "c:/wamp64/www/";
+
+}
+else
+{
+	$repertoire = $_GET['path'];
 }
 
+/*  Fonction permettant d'accèder au dossier www  */
+function displayHome($repertoire)
+{
+	$dossier = scandir($repertoire);
+
+	for($i = 1 ; $i < count($dossier); $i++)
+	{
+		echo $dossier[$i];
+
+		/*  Vérifie si le dossier est vraiment un dossier  */
+		if(is_file($repertoire . $dossier[$i]) == false)
+		{
+			$chemin = $repertoire . $dossier[$i];
+			echo $chemin;
+			//echo "<a href='path=$chemin'><img src='css/images/arrow.png'></a><br>";
+		}
+		else
+		{
+			echo "c'est un fichier <br> <br>";
+		}
+	}
+}
+
+
+displayHome($repertoire);
+
+ //  $ma_chaine = 'C:/wamp64/www';
+ //  $trouve_moi  = '/';
+ //  $position = strripos($ma_chaine, $trouve_moi);
+
+ //  // Vous devez utiliser ===  car == n'affichera rien
+ //  // car la lettre 'a' est à la position 0
+
+ //  if ($position === false) {
+ //      echo '',$trouve_moi,' n\'a pas été trouvé dans la chaine ',$ma_chaine,'';
+ //      } else 
+ //      {
+ //      echo '',$trouve_moi,' a été trouvée dans la chaîne ',$ma_chaine,'';
+ //      echo 'à la position ',$position,'<br>';
+ //      }
+ // echo substr($ma_chaine, 0, $position - strlen($ma_chaine));
+
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lecteur de fichiers</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<a href="?path=c:/wamp64/www"><img src="css/images/home.png" alt="HOME"></a> 
+</body>
+</html>
